afa373d5aead168fbc68acde1c2d1c1972e46959
DJ Bates <blooviper@gmail.com>
2022-08-04 09:30:34 -0600

    First commit

---
+++ VariableIntro/VariableIntroConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace VariableIntroConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Declaration and Initialization 
+            int myAge = 26;
+            string myName = "DJ";
+            bool isMarried = false;
+            string homeTown = "Pocatello, Idaho";
+
+            Console.WriteLine($"Hi my name is {myName}, and I am {myAge} years old!");
+            Console.WriteLine($"I am orignally from {homeTown}");
+            
+        }
+    }
+}
