6d7231f16bf3ce980f6abe59589e423b17c979e8
DJbeats182 <110370872+DJbeats182@users.noreply.github.com>
2022-08-05 14:15:24 -0600

    Update Program.cs

--- VariableIntro/VariableIntroConsole/Program.cs
+++ VariableIntro/VariableIntroConsole/Program.cs
@@ -7,14 +7,17 @@
             // Variable Declaration and Initialization 
             int myAge = 26;
             string myName = "DJ";
-            bool isMarried = false;
+            bool enjoyCoding = true;
             string homeTown = "Pocatello, Idaho";
             char andSymbol = '&';
-            decimal pi = 3.14m;
-            double randomNumber = 6.23456;
+            decimal pi = 3.14159255359m;
+            double randomNumber = 6.23;
 
             Console.WriteLine($"Hi my name is {myName}, and I am {myAge} years old!");
             Console.WriteLine($"I am orignally from {homeTown}");
+            Console.WriteLine($"My two favorite numbers are {pi} {andSymbol} {randomNumber}");
+            Console.WriteLine($"When asked if I enjoy coding? I would reply {enjoyCoding}");
+            
             
         }
     }
