0dea5a68e911e391ea0ea41bd074acb7ca4129d7
DJ Bates <blooviper@gmail.com>
2022-08-05 12:27:30 -0600

    Added decimal, char, and double

--- VariableIntro/VariableIntroConsole/Program.cs
+++ VariableIntro/VariableIntroConsole/Program.cs
@@ -9,6 +9,9 @@
             string myName = "DJ";
             bool isMarried = false;
             string homeTown = "Pocatello, Idaho";
+            char = '&';
+            decimal pi = 3.14m;
+            double randomNumber = 6.23456;
 
             Console.WriteLine($"Hi my name is {myName}, and I am {myAge} years old!");
             Console.WriteLine($"I am orignally from {homeTown}");
